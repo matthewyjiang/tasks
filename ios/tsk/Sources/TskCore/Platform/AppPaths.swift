a32fa396bebda3d0b0fdb56033f5c71c334a4a94
Matthew Jiang <matthewyjiang@gmail.com>
2026-06-14 20:53:20 -0700

    fix(ios): preserve stable app storage path

--- ios/tsk/Sources/TskCore/Platform/AppPaths.swift
+++ ios/tsk/Sources/TskCore/Platform/AppPaths.swift
@@ -2,18 +2,20 @@ import Foundation
 
 public struct AppPaths: Equatable, Sendable {
     public var bundleIdentifier: String
+    public var storageIdentifier: String
     public var applicationSupport: URL
     public var caches: URL
     public var temporary: URL
 
-    public init(fileManager: FileManager = .default, bundleIdentifier: String? = nil) throws {
-        let resolvedBundleIdentifier = bundleIdentifier ?? Bundle.main.bundleIdentifier ?? "tsk"
+    public init(fileManager: FileManager = .default, bundleIdentifier: String? = nil, storageIdentifier: String = "tsk") throws {
+        let resolvedBundleIdentifier = bundleIdentifier ?? Bundle.main.bundleIdentifier ?? "com.matthewyjiang.tsk"
         let supportRoot = try fileManager.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
         let cacheRoot = try fileManager.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
         self.bundleIdentifier = resolvedBundleIdentifier
-        applicationSupport = supportRoot.appending(path: resolvedBundleIdentifier, directoryHint: .isDirectory)
-        caches = cacheRoot.appending(path: resolvedBundleIdentifier, directoryHint: .isDirectory)
-        temporary = fileManager.temporaryDirectory.appending(path: resolvedBundleIdentifier, directoryHint: .isDirectory)
+        self.storageIdentifier = storageIdentifier
+        applicationSupport = supportRoot.appending(path: storageIdentifier, directoryHint: .isDirectory)
+        caches = cacheRoot.appending(path: storageIdentifier, directoryHint: .isDirectory)
+        temporary = fileManager.temporaryDirectory.appending(path: storageIdentifier, directoryHint: .isDirectory)
     }
 
     public var databaseURL: URL {
